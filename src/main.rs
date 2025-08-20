817da245ad7a921eff0724b62276643d8b02fd85
imgoby <imgoby@163.com>
2025-08-20 16:12:33 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use std::fs::File;
+use std::io::BufWriter;
+use std::io::Write;
+
+use mysql::prelude::Queryable;
+use mysql::Pool;
+
+
+#[derive(Debug)]
+struct Item {
+    id: u64,
+    keyword: String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let manticore_pool = Pool::new("mysql://root:@192.168.0.211:9306/Manticore?reset_connection=false").unwrap();
+
+    let mut conn = manticore_pool.get_conn().unwrap();
+
+    let file = File::create("mobile.txt").unwrap();
+    let mut writer = BufWriter::new(file);
+
+    loop {
+        let mut scroll:String="".to_string();
+        if let Some(val) = conn.query_first::<String,&str>("SHOW SCROLL;").unwrap(){
+            print!("{}",scroll);
+            scroll=val;
+        }
+
+        let mut sql:String="select id,keyword from req_log where sp_id='200002' and req_date>='2025-07-01' and req_date<'2025-08-01' order by id asc limit 10000; ".to_string();
+        if scroll.len()>0{
+            sql.push_str(&scroll);
+        }
+
+        let items = conn.query_map(sql, |(id, keyword)| {
+            Item {
+                id,
+                keyword
+            }
+        }).unwrap();
+
+        if items.len()==0{
+            print!("end.");
+            break;
+        }
+
+        for item in items{
+            let line=format!("{},{}\n",item.id,item.keyword);
+            writer.write_all(line.as_bytes()).unwrap();
+        }
+    }
 }
